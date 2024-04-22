e1301f9de982a385f5f320384f1d4d304b46a1e3
George Chrysafidis <george@overronet.com>
2024-04-22 17:01:09 +0300

    database Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numRevies=random_int(5,30);
+        });
 }
