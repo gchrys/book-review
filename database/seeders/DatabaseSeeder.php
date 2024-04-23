b12d5ebbfb471be9bdfcdbb55b521abc85b15b3c
George Chrysafidis <george@overronet.com>
2024-04-23 16:57:27 +0300

    relations and tinker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
+use App\Models\Review;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Book::factory(33)->create()->each(function (Book $book) {
-            $numRevies=random_int(5,30);
+            $numReviews=random_int(5,30);
+
+            Review::factory()->count($numReviews)->good()->for($book)->create();
+        });
+
+        Book::factory(34)->create()->each(function (Book $book) {
+            $numReviews=random_int(5,30);
+
+            Review::factory()->count($numReviews)->average()->for($book)->create();
+        });
+
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews=random_int(5,30);
+
+            Review::factory()->count($numReviews)->bad()->for($book)->create();
         });
     }
 }
