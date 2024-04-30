cfc1153d298e8ca242a9684dbf9528b4fc9404a6
George Chrysafidis <george@overronet.com>
2024-04-30 16:57:53 +0300

    filtering and single book page template

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,28 @@
 @section('content')
 <h1 class="mb-10 text-2xl">Books</h1>
 
-<form></form>
+<form method="get" action="{{ route('books.index')}}" class="mb-4 flex items-center space-x-2">
+    <input class="input h-10" type="text" name="title" placeholder="Search by tile" value="{{ request('title')}}" />
+    <input type="hidden" name="filter" value="{{request('filter')}}" />
+    <button type="submit" class="btn h-10">Search</button>
+    <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
+</form>
+
+<div class="filter-container mb-4 flex">
+@php
+    $filters = [
+        '' => 'Latest',
+        'popular_last_month' => 'Popular Last Month',
+        'popular_last_6months' => 'Popular Last 6 Months',
+        'highest_rated_last_month' => 'Highest Rated Last Month',
+        'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
+    ]
+@endphp
+
+@foreach ($filters as $key=>$label )
+    <a href="{{route('books.index',[...request()->query(),'filter'=>$key])}}" class="{{ request('filter') === $key || (request('filter')=== null && $key==='') ? 'filter-item-active' : 'filter-item'}}">{{$label}}</a>
+@endforeach
+</div>
 
 <ul>
     @forelse ($books as $book)
@@ -11,22 +32,27 @@
             <div class="book-item">
                 <div class="flex flex-wrap items-center justify-between">
                 <div class="w-full flex-grow sm:w-auto">
-                    <a href="{{route('books.show',['book'=>$book->id])}}" class="book-title">{{$book->title}}</a>
-                    <span class="book-author">{{$book->author}}</span>
+                    <a href="{{route('books.show',$book)}}" class="book-title">{{$book->title}}</a>
+                    <span class="book-author">by {{$book->author}}</span>
                 </div>
                 <div>
                     <div class="book-rating">
-                    {{$book->rating}}
+                    {{number_format($book->reviews_avg_rating,1)}}
                     </div>
                     <div class="book-review-count">
-                    out of {{$book->review_count}} reviews
+                    out of {{$book->reviews_count}} {{Str::plural('review',$book->reviews_count)}}
                     </div>
                 </div>
                 </div>
             </div>
         </li>
     @empty
-        
+    <li class="mb-4">
+        <div class="empty-book-item">
+            <p class="empty-text">No books found</p>
+            <a href="{{route('books.index')}}" class="reset-link">Reset criteria</a>
+        </div>
+    </li>
     @endforelse
 </ul>
 @endsection
